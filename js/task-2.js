d7be89851c0c556019ae4c2a0dffadccf73e97c2
Oleh_Denysenko <Oleh.Denysenkoo@gmail.com>
2025-01-10 14:54:19 +0200

    correct task 2

--- js/task-2.js
+++ js/task-2.js
@@ -14,7 +14,7 @@ function formatMessage(message, maxLength) {
         return `${message}`;
     }
     else {
-        return `${message.slice(maxLength)} ...`;
+        return `${message.slice(0, maxLength)} ...`;
     }
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
